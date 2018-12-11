757f9662272ae227884b11af6d8e9bfae4a05c72
Ivo Strejc <strejivo@fit.cvut.cz>
2018-12-11 09:00:25 +0100

    Added day 11 solutions, could be made MUCH faster with partial sum array

--- 11/advanced.cpp
+++ 11/advanced.cpp
@@ -2,10 +2,45 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <array>
+
+#define SERIAL_NUMBER 5791
 
 using namespace std;
 
+int getPowerLevel(int x, int y) {
+	int rackId = x + 10;
+	int powerLevel = rackId * y + SERIAL_NUMBER;
+	powerLevel *= rackId;
+	powerLevel = (powerLevel % 1000) / 100;
+	return powerLevel - 5;
+}
+
 int main() {
-	ifstream ifs("input.txt");
+	array<array<int, 300>, 300> board;
+	for (size_t x = 0; x < 300; x++){
+		for (size_t y = 0; y < 300; y++){
+			board[x][y] = getPowerLevel(x, y);
+		}
+	}
+	int maxFuel = numeric_limits<int>::min();
+	size_t maxX, maxY, maxSize = maxY = maxX = 0;
+	for (size_t x = 0; x <300; x++){
+		for (size_t y = 0; y < 300; y++){
+			for (size_t squareSize = 1; squareSize < 300 - max(x, y); squareSize ++) {
+				int fuelSum = 0;
+				for (size_t i = 0; i < squareSize; i++)
+					for (size_t j = 0; j < squareSize; j++)
+						fuelSum += board[x + i][y + j];
+				if (fuelSum > maxFuel) {
+					maxFuel = fuelSum;
+					maxX = x;
+					maxY = y;
+					maxSize = squareSize;
+				}
+			}
+		}
+	}
+	cout<<board[maxX][maxY]<<" at "<<maxX<<","<<maxY<<","<<maxSize<<endl;
 	return 0;
 }
--- 11/basic.cpp
+++ 11/basic.cpp
@@ -2,10 +2,42 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <array>
+
+#define SERIAL_NUMBER 5791
 
 using namespace std;
 
+int getPowerLevel(int x, int y) {
+	int rackId = x + 10;
+	int powerLevel = rackId * y + SERIAL_NUMBER;
+	powerLevel *= rackId;
+	powerLevel = (powerLevel % 1000) / 100;
+	return powerLevel - 5;
+}
+
 int main() {
-	ifstream ifs("input.txt");
+	array<array<int, 300>, 300> board;
+	for (size_t x = 0; x < 300; x++){
+		for (size_t y = 0; y < 300; y++){
+			board[x][y] = getPowerLevel(x, y);
+		}
+	}
+	int maxFuel = numeric_limits<int>::min();
+	int maxX, maxY = maxX = 0;
+	for (size_t x = 0; x <298; x++){
+		for (size_t y = 0; y < 298; y++){
+			int fuelSum = 0;
+			for (size_t i = 0; i < 3; i++)
+				for (size_t j = 0; j < 3; j++)
+					fuelSum += board[x+i][y+j];
+			if (fuelSum > maxFuel){
+				maxFuel = fuelSum;
+				maxX = x;
+				maxY = y;
+			}
+		}
+	}
+	cout<<board[maxX][maxY]<<" at "<<maxX<<","<<maxY<<endl;
 	return 0;
 }
